469352cdcb7fe8d71a38f619faaaa79e89a1340c
jonsoler <jon.soler@opendeusto.es>
2022-04-03 12:00:19 +0200

    Comprobar campos del registro, paquete server

--- src/main/java/es/deusto/spq/client/gui/VentanaRegistro.java
+++ src/main/java/es/deusto/spq/client/gui/VentanaRegistro.java
@@ -11,6 +11,7 @@ import java.lang.ModuleLayer.Controller;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
@@ -138,7 +139,75 @@ public class VentanaRegistro extends JFrame {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
-				
+				boolean correcto = false;
+				boolean error = false;
+				String dni = textoDNI.getText().toString();
+				String nombre = textoNombre.getText().toString();
+				String apellido = textoApellido.getText().toString();
+				String edad = textoEdad.getText().toString();
+
+				if (textoDNI.getText().equals("") || textoNombre.getText().equals("") || textoApellido.getText().equals("")
+						|| textoEmail.getText().equals("") || textoContrasenya.toString().equals("")
+						|| textoConfirmarContrasenya.toString().equals("")) {
+
+					JOptionPane.showMessageDialog(null, "Por favor, rellene todos los campos.", "Error",JOptionPane.INFORMATION_MESSAGE);
+					VentanaRegistro.this.repaint();
+					error = true;
+
+				} else if (dni.matches("^[a-zA-Z]+$")) {
+
+					JOptionPane.showMessageDialog(null, "DNI no válido.", "Error",JOptionPane.INFORMATION_MESSAGE);
+					error = true;
+					VentanaRegistro.this.repaint();
+
+				} else if (nombre.matches("^[0-9]+$")) {
+
+					JOptionPane.showMessageDialog(null, "Nombre no válido.", "Error",JOptionPane.INFORMATION_MESSAGE);
+					error = true;
+					VentanaRegistro.this.repaint();
+
+				} else if (apellido.matches("^[0-9]+$")) {
+
+					JOptionPane.showMessageDialog(null, "Apellido no válido.", "Error",JOptionPane.INFORMATION_MESSAGE);
+					error = true;
+					VentanaRegistro.this.repaint();
+					
+				} else if (edad.matches("^[0-99]")) {
+
+					JOptionPane.showMessageDialog(null, "Edad no válida.", "Error",JOptionPane.INFORMATION_MESSAGE);
+					error = true;
+					VentanaRegistro.this.repaint();
+
+				} else if (!textoEmail.getText().contains("@") || !(textoEmail.getText().contains(".es")
+						|| textoEmail.getText().contains(".com") || textoEmail.getText().contains(".eus"))) {
+
+					JOptionPane.showMessageDialog(null, "Email no válido.", "Error",JOptionPane.INFORMATION_MESSAGE);
+					error = true;
+					VentanaRegistro.this.repaint();
+
+				} else if (!String.valueOf(textoContrasenya.getPassword())
+						.equals(String.valueOf(textoConfirmarContrasenya.getPassword()))) {
+
+					JOptionPane.showMessageDialog(null, "Las contraseñas no coinciden.", "Error",JOptionPane.INFORMATION_MESSAGE);
+					VentanaRegistro.this.repaint();
+					error = true;
+
+				} else if (!error) {
+
+					//Codigo para registrar el usuario
+				}
+
+				if (correcto) {
+					JOptionPane.showMessageDialog(null, "Cliente registrado correctamente.", "Nuevo cliente",JOptionPane.INFORMATION_MESSAGE);
+					VentanaLogin inicio = new VentanaLogin(controller);
+					inicio.setVisible(true);
+					VentanaRegistro.this.dispose();
+
+				} else if (!error) {
+					JOptionPane.showMessageDialog(null,
+							"El email introducido ya ha sido registrado, pruebe a iniciar sesión.", "Error",JOptionPane.INFORMATION_MESSAGE);
+					VentanaRegistro.this.repaint();
+				}
 			}
 		});
 
---
+++ src/main/java/es/deusto/spq/server/Main.java
@@ -0,0 +1,10 @@
+package es.deusto.spq.server;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
