f135707746f4c43b4637006c84aea912a47473f7
Yon Ander Escobal Fernández <73309512+yonan27@users.noreply.github.com>
2022-04-03 17:48:02 +0200

    Update ReservaMaterial.java
    Clase reserva material completa

--- src/main/java/es/deusto/spq/client/ReservaMaterial.java
+++ src/main/java/es/deusto/spq/client/ReservaMaterial.java
@@ -1,12 +1,30 @@
 package es.deusto.spq.client;
 
+import javax.jdo.annotations.Inheritance;
+import javax.jdo.annotations.InheritanceStrategy;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.PrimaryKey;
+
+@PersistenceCapable
+@Inheritance(strategy=InheritanceStrategy.NEW_TABLE)
+
 public class ReservaMaterial {
+	@PrimaryKey
 	private String IDMaterial;
     private Cliente Cliente;
     private int anyo;
     private int mes;
     private int dia;
     private int hora;
+    
+    /** Constructor de la clase reservas
+     * @param IDMaterial ID del material
+     * @param Cliente que realiza la reserva
+     * @param anyo Año de la reserva
+     * @param mes Mes de la reserva
+     * @param dia Día de la reserva
+     * @param hora Hora de la reserva
+     */
 	
     public ReservaMaterial(String iDMaterial, es.deusto.spq.client.Cliente cliente, int anyo, int mes, int dia,
 			int hora) {
