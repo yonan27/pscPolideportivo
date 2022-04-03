d7d455883e6630bae56939eccdeafd0bd1c41546
markelmorales15 <markeluribe@opendeusto.es>
2022-04-03 18:52:31 +0200

    Base de datos de instalacion con objetos metidos

---
+++ src/main/sql-BD/instalaci__n.sql
@@ -0,0 +1,56 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 03-04-2022 a las 18:45:45
+-- Versión del servidor: 10.4.22-MariaDB
+-- Versión de PHP: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `mysql`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `instalación`
+--
+
+CREATE TABLE `instalación` (
+  `ID` varchar(11) NOT NULL,
+  `Nombre` varchar(30) NOT NULL,
+  `Precio` int(3) NOT NULL,
+  `Libre` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `instalación`
+--
+
+INSERT INTO `instalación` (`ID`, `Nombre`, `Precio`, `Libre`) VALUES
+('1A', 'Campo de futbol', 20, 0),
+('1A', 'Campo de futbol', 20, 0),
+('1B', 'Campo de baloncesto', 25, 0),
+('1B', 'Campo de baloncesto', 25, 0),
+('1C', 'Pista de tenis ', 15, 0),
+('1D', 'Pista de padel', 15, 0),
+('1C', 'Pista de tenis ', 15, 0),
+('1D', 'Pista de padel', 15, 0),
+('1C', 'Pista de tenis ', 15, 0),
+('1D', 'Pista de padel', 15, 0);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
